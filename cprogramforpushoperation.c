779d486b3b43e0a0983e97436c6a378d29c998fa
Mandeep <120255223+Mandeepkaur26@users.noreply.github.com>
2023-04-22 20:19:07 +0530

    Add files via upload

---
+++ cprogramforpushoperation.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int top,stack[top],n,data;
+if(top==n)
+{
+printf("\n overflow");
+}
+else
+{
+     printf("\n enter the element");
+     scanf("%d",&n);
+
+     top=top+1;
+
+     stack[top]=data;
+
+     return 0;
+}
+}
